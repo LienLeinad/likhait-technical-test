05297970bb00cb71557d5c61b7a594feba5ecae4
Neil Bautista <neil.bau@tista.net>
2026-07-24 22:46:36 -0700

    feat: add create route and controller

--- backend/app/controllers/api/categories_controller.rb
+++ backend/app/controllers/api/categories_controller.rb
@@ -3,4 +3,29 @@ class Api::CategoriesController < ApplicationController
     categories = Category.order(:name)
     render json: categories
   end
+  
+  def create
+    category = Category.new(category_params)    
+    if category.save
+      # Success
+      render json: format_category(category), status: :created
+    else
+      # Fail
+      render json: { errors: categories.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def format_category(category)
+    {
+      id: category.id,
+      name: category.name,
+      created_at: expense.created_at,
+      updated_at: expense.updated_at,
+    }
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
+
